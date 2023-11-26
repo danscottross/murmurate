8b1af30246409cc27b92c44a19122d7a29007001
Dan Ross <danscottross@gmail.com>
2023-11-26 12:54:11 -0700

    Ingest code v1

---
+++ Words/words_ingest.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import io
+import pickle
+
+with open('/Users/danross/Downloads/words.csv') as f:
+    words_raw = f.read()
+
+csv = io.StringIO(words_raw)
+df = pd.read_csv(csv, names=["Word", "Meaning"])
+
+df.to_pickle('/Users/danross/Desktop/murmurate/Words/words.pkl')  # where to save it, usually as a .pkl
+
+print(df)
--- proto.py
+++
@@ -1,3 +0,0 @@
-print("murmurate1")
-print("edit in VSC")
-print("branch1")
