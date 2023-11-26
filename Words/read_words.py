16b6563663ea38e352b840a857873d3167fac72e
Dan Ross <danscottross@gmail.com>
2023-11-26 13:02:08 -0700

    Push read_words

---
+++ Words/read_words.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_pickle('Words/words.pkl')
+
+print(df)
--- Words/words_ingest.py
+++ Words/words_ingest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import io
 import pickle
 
-with open('/Users/danross/Downloads/words.csv') as f:
+with open('Words/words.csv') as f:
     words_raw = f.read()
 
 csv = io.StringIO(words_raw)
