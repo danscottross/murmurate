2592550b65a3c09071193a32d478305ed18d48ef
Dan <danscottross@gmail.com>
2024-04-17 14:12:09 -0600

    Ignore Google API Key

--- words/get_words_from_sheets.py
+++ words/get_words_from_sheets.py
@@ -1,1 +1,10 @@
+import gspread
 
+gc = gspread.service_account(filename='murmurate-email-34f60d2ba6cd.json')
+
+wks = gc.open_by_key('1uBARUdToGUDsreuGWSO5s7Nq528Szo3o1ET589wS_Sg')
+sheet = wks.get_worksheet(0)
+
+output = sheet.cell(2,2).value
+
+print(output)
