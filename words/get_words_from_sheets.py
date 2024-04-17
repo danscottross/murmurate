630ad049e296cafa2f750eae7100a2ec40552cf8
Dan <danscottross@gmail.com>
2024-04-17 14:56:56 -0600

    Update

--- words/get_words_from_sheets.py
+++ words/get_words_from_sheets.py
@@ -1,7 +1,7 @@
 import gspread
 import pandas as pd
 
-gc = gspread.service_account(filename='murmurate-email-34f60d2ba6cd.json')
+gc = gspread.service_account(filename='murmurate/murmurate-email-34f60d2ba6cd.json')
 
 wks = gc.open_by_key('1uBARUdToGUDsreuGWSO5s7Nq528Szo3o1ET589wS_Sg')
 sheet = wks.get_worksheet(0)
@@ -12,5 +12,7 @@ for col in range(1, len(sheet.row_values(1)) + 1):
     word_dict[sheet.col_values(col)[0]] = sheet.col_values(col)[1:]
 
 df = pd.DataFrame(word_dict)
+
+# df.sort_values('Last User', ascending=False, inplace = True)
 
 print(df)
